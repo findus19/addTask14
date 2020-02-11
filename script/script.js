e73cbef9e6212026c54eddc676b15d2b92a326d1
findus19 <ivanexs@mail.ru>
2020-02-11 21:38:07 +0300

    addtask02

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 'use strict'
 class DomElement {
     constructor(height, width, backgroundColor, fontSize) {
-        this.selector = '#block'
+        this.selector = '.block'
         this.height = height;
         this.width = width;
         this.bg = backgroundColor;
@@ -29,6 +29,68 @@ class DomElement {
         };
     };
 };
-const domElement = new DomElement('100px', '100px', 'blue', '24px');
+const domElement = new DomElement('100px', '100px', '#05dae6', '24px');
+domElement.makeNewElem();
 
-console.log(domElement.makeNewElem());
+const squer = document.querySelector('.block');
+let widthCl = document.documentElement.clientWidth;
+let heightCl = document.documentElement.clientHeight;
+squer.style.position = 'absolute';
+
+squer.style.left = "45%";
+squer.style.top = "45%";
+
+document.addEventListener('keydown', startRun);
+document.addEventListener('keyup', stopRun);
+
+const keys = {
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowRight: false,
+    ArrowLeft: false
+};
+
+let settingX = squer.offsetLeft;
+let settingY = squer.offsetTop;
+let settingSpeed = 50;
+requestAnimationFrame(playGame);
+
+function playGame() {
+    if(keys.ArrowLeft && settingX > 0){
+        settingX -= settingSpeed;
+    }
+
+    if(keys.ArrowRight && settingX < widthCl){
+        settingX += settingSpeed;
+    }
+
+    if(keys.ArrowUp && settingY > 0){
+        settingY -= settingSpeed;
+    }
+
+    if(keys.ArrowDown && settingY < heightCl){
+        settingY += settingSpeed;
+    }
+
+    
+    squer.style.left = settingX + 'px';
+    squer.style.top = settingY + 'px';
+    requestAnimationFrame(playGame);
+/*     console.log(keys.ArrowDown);
+console.log(settingX);
+console.log(settingY); */
+}
+console.log(heightCl);
+function startRun(event){
+    event.preventDefault();
+    if(keys.hasOwnProperty(event.key)){
+    keys[event.key] = true;
+    }
+}
+
+function stopRun(event){
+    event.preventDefault();
+    if(keys.hasOwnProperty(event.key)){
+        keys[event.key] = false;
+    }
+}
